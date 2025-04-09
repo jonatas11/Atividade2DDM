8a5ae24edd7459e96ac9a054da1f8687bea2da89
Jonatas <jonatas11fallen@gmail.com>
2025-04-08 21:42:23 -0300

    Feito o diferencial proposto

--- src/app/pedra-papel-tesoura/pedra-papel-tesoura.page.ts
+++ src/app/pedra-papel-tesoura/pedra-papel-tesoura.page.ts
@@ -13,6 +13,10 @@ export class PedraPapelTesouraPage {
   numeroComputador: number = 0;
   resultado: string = '';
   escolhaComputador: string = '';
+  contadorUsuario: number = 0;
+  contadorPC: number = 0;
+  placar: string = '';
+
   jogar() {
     this.numeroComputador = Math.floor(Math.random() * 3) + 1;
 
@@ -33,34 +37,42 @@ export class PedraPapelTesouraPage {
 
     if (escolha === 1 && this.numeroComputador === 3) {
       this.resultado = `🎉 Parabéns!! Você ganhou! Minha escolha era ${this.escolhaComputador} e a sua Pedra`;
+      this.contadorUsuario++;
     } 
     else if (escolha === 1 && this.numeroComputador === 2) {
       this.resultado = `😢 Você perdeu! Minha escolha era ${this.escolhaComputador} e a sua Pedra`;
+      this.contadorPC++;
     }
     else if (escolha === 1 && this.numeroComputador === 1) {
       this.resultado = `😯 Ops... Empatamos! Minha escolha era ${this.escolhaComputador} e a sua Pedra`;
     }
     else if (escolha === 2 && this.numeroComputador === 3) {
       this.resultado = `😢 Você perdeu! Minha escolha era ${this.escolhaComputador} e a sua Papel`;
+      this.contadorPC++;
     } 
     else if (escolha === 2 && this.numeroComputador === 2) {
       this.resultado = `😯 Ops... Empatamos! Minha escolha era ${this.escolhaComputador} e a sua Papel`;
     }
     else if (escolha === 2 && this.numeroComputador === 1) {
       this.resultado = `🎉 Parabéns!! Você ganhou! Minha escolha era ${this.escolhaComputador} e a sua Papel`;
+      this.contadorUsuario++;
     }
     else if (escolha === 3 && this.numeroComputador === 3) {
       this.resultado = `😯 Ops... Empatamos! Minha escolha era ${this.escolhaComputador} e a sua Tesoura`;
     } 
     else if (escolha === 3 && this.numeroComputador === 2) {
       this.resultado = `🎉 Parabéns!! Você ganhou! Minha escolha era ${this.escolhaComputador} e a sua Tesoura`;
+      this.contadorUsuario++;
     }
     else if (escolha === 3 && this.numeroComputador === 1){
       this.resultado = `😢 Você perdeu! Minha escolha era ${this.escolhaComputador} e a sua Tesoura`;
+      this.contadorPC++;
     }
     else {
       this.resultado = `⚠️ Escolha inválida! Selecione uma das opções.`;
     }
+
+    this.placar = `Placar: Usuário ${this.contadorUsuario} X ${this.contadorPC} Computador`
     
   }
 
